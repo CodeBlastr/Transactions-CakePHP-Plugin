1daebe86a0374eca821db4f196a233e6a823fe0b
Doug Eaton <doug@buildrr.com>
2013-10-02 16:22:12 -0400

    Changing to Live
    runin live

--- Model/Processor/Bluepay.php
+++ Model/Processor/Bluepay.php
@@ -185,19 +185,19 @@ class Bluepay extends AppModel {
 			sprintf('%02d', $data['Transaction']['card_exp_month']) . substr($data['Transaction']['card_exp_year'], 2), // $this->expire
 			$data['TransactionAddress'][0]['first_name'], // $this->name1
 			$data['TransactionAddress'][0]['last_name'], // $this->name2 
-			$data['TransactionAddress']['street_address_1'], //$this->addr1 
-			$data['TransactionAddress']['city'], //$this->city
-			$data['TransactionAddress']['state'], //$this->state 
-			$data['TransactionAddress']['zip'], //$this->zip
-			$data['TransactionAddress']['country'], //$this->country
-			$data['TransactionAddress']['phone'], //$this->phone
-			$data['TransactionAddress']['email'], //$this->email 
+			$data['TransactionAddress'][0]['street_address_1'], //$this->addr1 
+			$data['TransactionAddress'][0]['city'], //$this->city
+			$data['TransactionAddress'][0]['state'], //$this->state 
+			$data['TransactionAddress'][0]['zip'], //$this->zip
+			$data['TransactionAddress'][0]['country'], //$this->country
+			$data['TransactionAddress'][0]['phone'], //$this->phone
+			$data['TransactionAddress'][0]['email'], //$this->email 
 			$data['Transaction']['customid1' == null], //$this->customid1 = null 
 			$data['Transaction']['customid2' == null], //$this->customid2 = null 
-			$data['TransactionAddress']['street_adress_2' == null], //$this->addr2 = null 
+			$data['TransactionAddress'][0]['street_adress_2' == null], //$this->addr2 = null 
 			$data['Transaction']['memo'] //$this->memo = null
 			);
-		$this->sale($data['Transaction']['total']);
+		$this->sale('1.00');
 		$this->process();
 		
 		
