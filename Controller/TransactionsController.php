0c2c3ed017f879c0e902b9376466e04cf8f4e260
Joel Byrnes <joel@razorit.com>
2013-12-16 18:07:30 +0000

    formatting

--- Controller/TransactionsController.php
+++ Controller/TransactionsController.php
@@ -247,28 +247,28 @@ class AppTransactionsController extends TransactionsAppController {
 				}
 			}
 		}
-				//// Interswitch
-				if(isset($this->request->data['Interswitch']['order_number']) && isset($this->request->data['Interswitch']['cart_order_id'])){
-                        // This user probably coming back from hitting OK at Interswitch.
-                        // Verify payment
-                        App::uses('Interswitch', 'Transactions.Model/Processor');
-                        $this->Processor = new Interswitch;
-                        $this->Processor->executePayment($this->request->data['Interswitch']);
-                        // Run the afterPayment callbacks.
-                        $data = CakeSession::read('Transaction.data');
-                        if (!empty($data)) {
-                                $this->Transaction->afterSuccessfulPayment(CakeSession::read('Auth.User.id'), $data);
+		//// Interswitch
+		if(isset($this->request->data['Interswitch']['order_number']) && isset($this->request->data['Interswitch']['cart_order_id'])){
+                // This user probably coming back from hitting OK at Interswitch.
+                // Verify payment
+                App::uses('Interswitch', 'Transactions.Model/Processor');
+                $this->Processor = new Interswitch;
+                $this->Processor->executePayment($this->request->data['Interswitch']);
+                // Run the afterPayment callbacks.
+                $data = CakeSession::read('Transaction.data');
+                if (!empty($data)) {
+                        $this->Transaction->afterSuccessfulPayment(CakeSession::read('Auth.User.id'), $data);
+                }
+				
+                $boughtModel = CakeSession::read('Transaction.modelName');
+                if (!empty($boughtModel)) {
+                        App::uses($boughtModel, ZuhaInflector::pluginize($boughtModel).'.Model');
+                        $Model = new $boughtModel;
+                        if (method_exists($Model, 'afterSuccessfulPayment') && is_callable('afterSuccessfulPayment')) {
+                                $Model->afterSuccessfulPayment(CakeSession::read('Auth.User.id'), $data);
                         }
-						
-                        $boughtModel = CakeSession::read('Transaction.modelName');
-                        if (!empty($boughtModel)) {
-                                App::uses($boughtModel, ZuhaInflector::pluginize($boughtModel).'.Model');
-                                $Model = new $boughtModel;
-                                if (method_exists($Model, 'afterSuccessfulPayment') && is_callable('afterSuccessfulPayment')) {
-                                        $Model->afterSuccessfulPayment(CakeSession::read('Auth.User.id'), $data);
-                                }
-                        }
-                } //end Interswitch
+                }
+        } //end Interswitch
                 
 		$this->set('userId', $this->Session->read('Auth.User.id'));
 	}
