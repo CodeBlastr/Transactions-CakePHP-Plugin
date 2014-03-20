0db20166e67c44c0ca842be41353ef97a55d7e17
Joel Byrnes <joel@razorit.com>
2014-03-20 10:54:21 -0400

    code cleanup

--- Controller/TransactionsAppController.php
+++ Controller/TransactionsAppController.php
@@ -4,9 +4,13 @@ class TransactionsAppController extends AppController {
 
 	public function beforeFilter() {
 		parent::beforeFilter();
-		if ($this->name == 'Transactions' && in_array('Tasks', CakePlugin::loaded())) {
-			$this->Transaction->TransactionItem->Behaviors->attach('Tasks.Assignable', array('notifyAssignee' => true, 'notifySubject' => 'New Order Assigned', 'notifyMessage' => 'Please login and view your assigned tasks, or orders to get the order details.'));
+		if ($this->name == 'Transactions' && CakePlugin::loaded('Tasks')) {
+			$this->Transaction->TransactionItem->Behaviors->attach('Tasks.Assignable', array(
+				'notifyAssignee' => true,
+				'notifySubject' => 'New Order Assigned',
+				'notifyMessage' => 'Please login and view your assigned tasks, or orders to get the order details.'
+			));
 		}
 	}
-    
+
 }
