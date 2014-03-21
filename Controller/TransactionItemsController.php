a51cba1c294d47a77dd6fbb01eb2b2a8af2ac27b
Joel Byrnes <joel@razorit.com>
2014-03-21 13:41:36 -0400

    add(): fix bug introduced in last commit

--- Controller/TransactionItemsController.php
+++ Controller/TransactionItemsController.php
@@ -56,7 +56,7 @@ class TransactionItemsController extends TransactionsAppController {
  * @throws NotFoundException
  */
 	public function add() {
-		if (!$this->request->is('post')) {
+		if ($this->request->is('post')) {
 			try {
 				if ($this->TransactionItem->addItemToCart($this->request->data)) {
 					$this->Session->setFlash(__d('transactions', 'Added to cart'), 'flash_success');
