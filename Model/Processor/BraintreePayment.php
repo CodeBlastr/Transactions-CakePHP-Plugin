a10ee8b63fe90e9fec3a8dd462621a0bbfe7e201
Buildrr LLC <richard@buildrr.com>
2015-06-16 13:31:18 +0000

    Added a new call to the api, that finds the customer list

--- Model/Processor/BraintreePayment.php
+++ Model/Processor/BraintreePayment.php
@@ -198,9 +198,18 @@ class BraintreePayment extends AppModel {
 
 /**
  * get customer list
+ * this looks like PaySimple but wasn't 100% sure (delete if everything is working)
  */
-	public function getCustomerList() {
-		return $this->_sendRequest('GET', '/customer');
+	// public function getCustomerList() {
+		// return $this->_sendRequest('GET', '/customer');
+	// }
+
+/**
+ * get customer
+ * 
+ */
+ 	public function getCustomer($customerId = null) {
+ 		return Braintree_Customer::find($customerId);
 	}
 
 /**
@@ -253,6 +262,30 @@ class BraintreePayment extends AppModel {
 			return false;
 		}
 	}
+
+/**
+ * get recurring payments
+ * https://developers.braintreepayments.com/javascript+php/reference/request/subscription/find
+ * 
+ */
+ 	public function getRecurringPayments($subscriptionId = null) {
+ 		return Braintree_Subscription::find($subscriptionId);
+	}
+
+/**
+ * find subscriptions by customer
+ * https://developers.braintreepayments.com/javascript+php/reference/request/customer/find
+ * https://developers.braintreepayments.com/javascript+php/reference/request/subscription/find
+ * 
+ */
+ 	public function getCustomerSubscriptions($customerId = null) {
+ 		$customer = $this->getCustomer($customerId);
+		$subscriptions = array();
+		foreach ($customer->creditCards as $card) {
+			$subscriptions = array_merge($subscriptions, $card->subscriptions);
+		}
+		return $subscriptions;
+ 	}
 	
 /**
  * @todo https://developers.braintreepayments.com/javascript+php/guides/recurring-billing/plans#add-ons-and-discounts
@@ -363,345 +396,6 @@ class BraintreePayment extends AppModel {
 		);
 		return $this->_sendRequest('POST', '/recurringpayment', $params);
 	}
-	
-/**
- * @link https://sandbox-api.paysimple.com/v4/Help/RecurringPayment#put-recurringpayment
- * 
- * @param array $data
- * @return boolean|array
- */
-	public function updateRecurringPayment($data) {
-		$params = array(
-			'CustomerId' => null,
-			'NextScheduleDate' => null,
-			'PauseUntilDate' => null,
-			'FirstPaymentDone' => null,
-			'DateOfLastPaymentMade' => null,
-			'TotalAmountPaid' => null,
-			'NumberOfPaymentsMade' => null,
-			'EndDate' => null, // updatable
-			'PaymentAmount' => null, // updatable
-			'PaymentSubType' => null, // updatable
-			'AccountId' => null, // updatable
-			'InvoiceNumber' => null,
-			'OrderId' => null,
-			'FirstPaymentAmount' => null, // updatable (if it hasn't started yet)
-			'FirstPaymentDate' => null, // updatable (if it hasn't started yet)
-			'StartDate' => null, // updatable (if it hasn't started yet)
-			'ScheduleStatus' => null,
-			'ExecutionFrequencyType' => null, // updatable
-			'ExecutionFrequencyParameter' => null, // updatable
-			'Description' => null,
-			'Id' => null
-		);
-		return $this->_sendRequest('PUT', '/recurringpayment', $params);
-	}
-	
-/**
- * @link https://sandbox-api.paysimple.com/v4/Help/RecurringPayment#put-recurringpayment-by-id-pause-until-enddate
- * 
- * @param array $data
- * @return boolean
- */
-	public function pauseRecurringPayment($data) {
-		return $this->_sendRequest('PUT', '/recurringpayment/'.$data['scheduleId'].'/pause?endDate='.$data['endDate']);
-	}
-	
-/**
- * @link https://sandbox-api.paysimple.com/v4/Help/RecurringPayment#put-recurringpayment-by-id-suspend
- * 
- * @param integer $scheduleId
- * @return boolean
- */
-	public function suspendRecurringPayment($scheduleId) {
-		return $this->_sendRequest('PUT', '/recurringpayment/'.$scheduleId.'/suspend');
-	}
-	
-/**
- * @link https://sandbox-api.paysimple.com/v4/Help/RecurringPayment#put-recurringpayment-by-id-resume
- * 
- * @param integer $data
- * @return boolean
- */
-	public function resumeRecurringPayment($data) {
-		return $this->_sendRequest('PUT', '/recurringpayment/'.$scheduleId.'/resume');
-	}
-	
-/**
- * Returns a list of Invoice records
- * @return type
- */
-	public function getInvoices() {
-		return $this->_sendRequest('GET', '/invoice');
-	}
-/**
- * Returns an Invoice record for the specified identifier
- * @param type $invoiceId
- * @return type
- */
-	public function getInvoice($invoiceId) {
-		return $this->_sendRequest('GET', '/invoice/'.$invoiceId);
-	}
-/**
- * Returns the next available invoice number when using auto-numbering within PaySimple
- * @return type
- */
-	public function getNextInvoiceNumber() {
-		return $this->_sendRequest('GET', '/invoice/number');
-	}
-/**
- * Gets all of the payments for the specified Invoice identifier
- * @param type $invoiceId
- * @return type
- */
-	public function getInvoicePayments($invoiceId) {
-		return $this->_sendRequest('GET', '/invoice/'.$invoiceId.'/payments');
-	}
-/**
- * Gets a list of actions on an Invoice given the specified identifier
- * @param type $invoiceId
- * @return type
- */
-	public function getInvoiceActions($invoiceId) {
-		return $this->_sendRequest('GET', '/invoice/'.$invoiceId.'/actions');
-	}
-/**
- * Returns a list of Line Item records for the specified Invoice identifier
- * @param type $invoiceId
- * @return type
- */
-	public function getInvoiceLineItems($invoiceId) {
-		return $this->_sendRequest('GET', '/invoice/'.$invoiceId.'/invoicelineitems');
-	}
-/**
- * Creates an Invoice record when provided with an Invoice object. This route does not immediately send the Invoice
- * @param type $data
- * @return type
- */
-	public function createInvoice($data) {
-		return $this->_sendRequest('POST', '/invoice', $data);
-	}
-/**
- * Updates an Invoice record when provided with an Invoice object
- * @param type $data
- * @return type
- */
-	public function updateInvoice($data) {
-		return $this->_sendRequest('PUT', '/invoice', $data);
-	}
-/**
- * Sends or resends an Invoice record given an identifier (via email)
- * @param type $invoiceId
- * @return type
- */
-	public function sendInvoice($invoiceId) {
-		return $this->_sendRequest('PUT', '/invoice/'.$invoiceId.'/send');
-	}
-/**
- * Marks an Invoice as paid or partially paid when provided with an Invoice identifier and a Received Payment object.
- * @param type $data
- * @return type
- */
-	public function addInvoicePayment($data) {
-		return $this->_sendRequest('PUT', '/invoice/'.$data['invoiceId'].'/externalpayment', $data);
-	}
-/**
- * Marks an Invoice paid when provided with an identifier. NOTE: A body is not required for this message.
- * @param type $invoiceId
- * @return type
- */
-	public function markInvoicePaid($invoiceId) {
-		return $this->_sendRequest('PUT', '/invoice/'.$invoiceId.'/markpaid');
-	}
-/**
- * Marks an Invoice unpaid when provided with an identifier. NOTE: A body is not required for this message.
- * @param type $invoiceId
- * @return type
- */
-	public function markInvoiceUnpaid($invoiceId) {
-		return $this->_sendRequest('PUT', '/invoice/'.$invoiceId.'/markunpaid');
-	}
-/**
- * Marks an Invoice as sent when provided with an identifier. NOTE: A body is not required for this message.
- * @param type $invoiceId
- * @return type
- */
-	public function markInvoiceSent($invoiceId) {
-		return $this->_sendRequest('PUT', '/invoice/'.$invoiceId.'/marksent');
-	}
-/**
- * Marks an Invoice as cancelled when provided with an identifier. NOTE: A body is not required for this message.
- * @param type $invoiceId
- * @return type
- */
-	public function markInvoiceCancelled($invoiceId) {
-		return $this->_sendRequest('PUT', '/invoice/'.$invoiceId.'/cancel');
-	}
-/**
- * Deletes an Invoice record when provided with an identifier. NOTE: A body is not required for this message.
- * @param type $invoiceId
- * @return type
- */
-	public function deleteInvoice($invoiceId) {
-		return $this->_sendRequest('DELETE', '/invoice/'.$invoiceId);
-	}
 
-	/**
- * @param integer $customerId
- * @return boolean|array
- */
-	public function findCustomerById($customerId) {
-		return $this->_sendRequest('GET', '/customer/' . $customerId);
-	}
-
-/**
- * try to find their email in the current customer list
- * @param string $email
- * @return boolean|array
- */
-	public function findCustomerByEmail($email) {
-		$customerList = $this->getCustomerList();
-		if ($customerList) {
-			foreach ($customerList as $customer) {
-				if ($customer['Email'] == $email) {
-					$user = $customer;
-					break;
-				}
-			}
-		}
-		if ($user) {
-			return $user;
-		} else {
-			return FALSE;
-		}
-	}
-
-/**
- * This function executes upon failure
- * @todo obsolete..?  seems that it may be.
- */
-	public function echoErrors() {
-		foreach ($this->errors as $error) {
-			$this->response['reason_text'] .= '<li>' . $error . '</li>';
-		}
-		$this->response['response_code'] = 0;
-		new Exception($this->response['reason_text']);
-	}
-
-/**
- *
- * @param type $cardNumber
- * @return boolean|integer
- */
-	public function getIssuer($cardNumber) {
-
-		App::uses('Validation', 'Utility');
-		if ( Validation::cc($cardNumber, array('visa')) ) {
-			$cardType = 'Visa';
-		} elseif ( Validation::cc($cardNumber, array('amex')) ) {
-			$cardType = 'Amex';
-		} elseif ( Validation::cc($cardNumber, array('mc')) ) {
-			$cardType = 'Master';
-		} elseif ( Validation::cc($cardNumber, array('disc')) ) {
-			$cardType = 'Discover';
-		} else {
-			$cardType = 'Unsupported';
-		}
-
-		$paySimpleCodes = array(
-			'Unsupported' => FALSE,
-			'Visa' => 12,
-			'Discover' => 15,
-			'Master' => 13,
-			'Amex' => 14,
-		);
-
-		return $paySimpleCodes[$cardType];
-	}
-
-/**
- * Prepares and sends your request to the API servers
- * 
- * @param string $method POST | GET | UPDATE | DELETE
- * @param string $action PaySimple API endpoint
- * @param array $data A PaySimple API Request Body packet as an array
- * @return boolean|array Returns Exception/FALSE or the "Response" array
- */
-	public function _sendRequest($method, $action, $data = NULL) {
-        if ($this->config['environment'] == 'sandbox') {
-			$endpoint = 'https://sandbox-api.paysimple.com/v4';
-		} else {
-			$endpoint = 'https://api.paysimple.com/v4';
-		}
-
-		$timestamp = gmdate("c");
-		$hmac = hash_hmac("sha256", $timestamp, $this->config['sharedSecret'], true); //note the raw output parameter
-		$hmac = base64_encode($hmac);
-
-		$request = array(
-			'method' => $method,
-			'uri' => $endpoint . $action,
-			'header' => array(
-				'Authorization' => "PSSERVER AccessId = {$this->config['apiUsername']}; Timestamp = {$timestamp}; Signature = {$hmac};"
-			),
-		);
-		if ($data !== NULL) {
-			$request['header']['Content-Type'] = 'application/json';
-			$request['header']['Content-Length'] = strlen(json_encode($data));
-			$request['body'] = json_encode($data);
-		}
-		$result = $this->Http->request($request);
-		return $this->_handleResult($result, $data);
-	}
-	
-	
-/**
- * 
- * @param Object $result An httpSocket response object
- * @param Array $data The PaySimple API Request Body packet as an array that was used for the request
- * @return Array The entire Response packet of a valid API call
- * @throws Exception The error message to display to the visitor
- */
-	private function _handleResult($result, $data) {
-		$responseCode = $result->code;
-		$badResponseCodes = array(400, 401, 403, 404, 405, 500);
-		// build error message
-		$result->body = json_decode($result->body, TRUE); // was in the if below;  // JOEL PLEASE CHECK IF THIS BROKE ANYTHING
-		
-		if (in_array($responseCode, $badResponseCodes)) {
-			if (isset($result->body['Meta']['Errors']['ErrorMessages'])) {
-				$message = '';
-				
-				//$message = $result['Meta']['Errors']['ErrorCode']. ' '; // this might be a redundant message
-				foreach ($result->body['Meta']['Errors']['ErrorMessages'] as $error) {
-					$this->errors[] = $error['Message'];
-					$message .= '<p>'.$error['Message'].'</p>';
-				}
-			} else {
-				$this->errors[] = $message = '<p>Uncaught error : 23692732470912876</p>' . ZuhaInflector::flatten($result);
-			}
-			
-//			// we need to know if this was an ARB that was declined ??
-//			if($data['Transaction']['is_arb']) {
-//				$arbErrorMessage = $result['Meta']['Errors']['ErrorMessages'][0]['Message'];
-//				if(strpos($arbErrorMessage, 'was saved, but the first scheduled payment failed')) {
-//					
-//				}
-//			}
-			
-			// some error logging perhaps?
-//			CakeLog::write('failed_transactions', $responseCode);
-//			CakeLog::write('failed_transactions', $request);
-//			CakeLog::write('failed_transactions', $result);
-			
-			// throw error message to display to the visitor
-
-			throw new Exception($message);
-			return FALSE;
-		} else {
-			// return entire Response packet of a valid API call
-			return $result->body['Response']; // was $result['Response']; // JOEL PLEASE CHECK IF THIS BROKE ANYTHING
-		}
-	}
 
 }
